a27b94bb7fb41c8546e5a6ec5bb70a31c86e56d4
Kamil Nahotko <kamilnahotko@gmail.com>
2024-08-25 19:00:09 +0200

    feat: integrate NeonDB with Drizzle ORM

---
+++ server/schema.ts
@@ -0,0 +1,6 @@
+import { serial, text, pgTable } from 'drizzle-orm/pg-core';
+
+export const posts = pgTable('posts', {
+  id: serial('id').primaryKey().notNull(),
+  title: text('title').notNull(),
+});
