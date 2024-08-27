1760c84d51e6950583b11210d3b5ce5145371c86
Kamil Nahotko <kamilnahotko@gmail.com>
2024-08-27 16:18:45 +0200

    feat: add user dropdown menu

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,14 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Inter as FontSans } from 'next/font/google';
 import './globals.css';
 
 import { Navigation } from '@/modules';
+import { cn } from '@/lib/utils';
 
-const inter = Inter({ subsets: ['latin'] });
+const fontSans = FontSans({
+  subsets: ['latin'],
+  variable: '--font-sans',
+});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -18,9 +22,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={inter.className}>
-        <Navigation />
-        {children}
+      <body className={cn('font-sans antialiased', fontSans.variable)}>
+        <div className='flex-grow px-6 md:px-12 mx-auto max-w-8xl'>
+          <Navigation />
+          {children}
+        </div>
       </body>
     </html>
   );
